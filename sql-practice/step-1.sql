2c9e3d2c3d57755342e2addfc6a6d194dc3e497a
Srivatsan <vatsan1993@gmail.com>
2024-10-09 17:34:57 +0530

    finished

--- sql-practice/step-1.sql
+++ sql-practice/step-1.sql
@@ -1,3 +1,6 @@
 -- Step 1
 -- JOIN the tables, matching FOREIGN KEYs to the corresponding PRIMARY KEY.
 -- Your code here
+select name, title
+from bands
+    join albums on bands.id = albums.band_id;
--- sql-practice/step-2.sql
+++ sql-practice/step-2.sql
@@ -2,3 +2,8 @@
 -- A WHERE clause can filter across any JOINed table, even if it's not in the
 -- final output of what is being SELECTed for.
 -- Your code here
+select name
+from bands
+    join albums on bands.id = albums.band_id
+where
+    num_sold < 20000;
